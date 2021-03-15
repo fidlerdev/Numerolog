139ec36d56e75c063e0c189358db8ff9149c83b8
Benjamin Fidler <fidlerdev@yandex.ru>
2021-03-15 04:17:26 +0300

    Некоторые изменения для отображения картинок в собранном приложении

--- load_resources.py
+++ load_resources.py
@@ -11,31 +11,40 @@ class Icons(QtGui.QIcon):
         QtGui.QIcon.__init__(self, path)
 
 def get_icons():
-    _icons_path = os.getcwd() + "{0}resources{0}icons{0}{1}{0}".format(os.sep, load_settings()["icons_set"])
+    
+    _root_folder = os.path.dirname(os.path.dirname(__file__))
+    _icons_path = os.path.join(_root_folder, 'resources', 'icons', str(load_settings()['icons_set']))
+    # _icons_path = os.getcwd() + "{0}resources{0}icons{0}{1}{0}".format(os.sep, load_settings()["icons_set"])
     print("icons_path: {}".format(_icons_path))
     # Записываем иконки в словарь объектов класса Icons
+    '''
+        TODO: 
+            -- Убрать ненужные иконки с os.getcwd()
+            -- Проработать навигацию папки resources
+            
+    '''
     icons = {
         "MAIN":                 Icons(os.getcwd() + "{0}resources{0}main_icon_100.png".format(os.sep)),
         "main":                 Icons(os.getcwd() + "{0}resources{0}main_icon_32.png".format(os.sep)),
         "main_beta_1":          Icons(os.getcwd() + "{0}resources{0}main_icon.png".format(os.sep)),
         "main_beta_2":          Icons(os.getcwd() + "{0}resources{0}main_beta_2.png".format(os.sep)),
-        "add_folder":           Icons(_icons_path + "add_folder.png"),
-        "add_user":             Icons(_icons_path + "add_user.png"),
-        "open_workspace":       Icons(_icons_path + "open_workspace.png"),
-        "close":                Icons(_icons_path + "close.png"),
-        "print":                Icons(_icons_path + "print.png"),
-        "user":                 Icons(_icons_path + "user.png"),
-        "folder":               Icons(_icons_path + "folder.png"),
-        "edit_file":            Icons(_icons_path + "edit_file.png"),
-        "rename":               Icons(_icons_path + "pencil.png"),
-        "calculate":            Icons(_icons_path + "calculate.png"),
-        "delete":               Icons(_icons_path + "delete.png"),
-        "warning":              Icons(_icons_path + "warning.png"),
-        "arrow_up":             Icons(_icons_path + "arrow_up.png"),
-        "arrow_down":           Icons(_icons_path + "arrow_down.png"),
-        "settings":             Icons(_icons_path + "settings.png"),
-        "open_list":            Icons(_icons_path + "open_list.png"),
-        "search":               Icons(_icons_path + "search.png")
+        "add_folder":           Icons(os.path.join(_icons_path, "add_folder.png")),
+        "add_user":             Icons(os.path.join(_icons_path, "add_user.png")),
+        "open_workspace":       Icons(os.path.join(_icons_path, "open_workspace.png")),
+        "close":                Icons(os.path.join(_icons_path, "close.png")),
+        "print":                Icons(os.path.join(_icons_path, "print.png")),
+        "user":                 Icons(os.path.join(_icons_path, "user.png")),
+        "folder":               Icons(os.path.join(_icons_path, "folder.png")),
+        "edit_file":            Icons(os.path.join(_icons_path, "edit_file.png")),
+        "rename":               Icons(os.path.join(_icons_path, "pencil.png")),
+        "calculate":            Icons(os.path.join(_icons_path, "calculate.png")),
+        "delete":               Icons(os.path.join(_icons_path, "delete.png")),
+        "warning":              Icons(os.path.join(_icons_path, "warning.png")),
+        "arrow_up":             Icons(os.path.join(_icons_path, "arrow_up.png")),
+        "arrow_down":           Icons(os.path.join(_icons_path, "arrow_down.png")),
+        "settings":             Icons(os.path.join(_icons_path, "settings.png")),
+        "open_list":            Icons(os.path.join(_icons_path, "open_list.png")),
+        "search":               Icons(os.path.join(_icons_path, "search.png"))
     }
     return icons
 
